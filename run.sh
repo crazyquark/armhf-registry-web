5bc3784ace2454028b423260a98a8db8f1260de5
crazyquark <cristian.sandu@gmail.com>
2017-03-22 21:50:33 +0200

    change registry url

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run -d -p 7080:8080 --name registry-web --link registry-v2:registry-srv -e REGISTRY_URL=http://dietpi:5000/v2 -e REGISTRY_NAME=dietpi:5000 crazyquark/armhf-registry-web
+docker run -d -p 7080:8080 --name registry-web --link registry-v2:registry-srv -e REGISTRY_URL=http://192.168.1.91:5000/v2 -e REGISTRY_NAME=dietpi:5000 crazyquark/armhf-registry-web:14.04
 
 
